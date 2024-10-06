693162e228c9fc55ed2736f155674c265947500f
batuhanerdeniz <modifci@gmail.com>
2024-10-06 14:36:16 +0300

    updated home page and added table

---
+++ assets/scripts/timings.js
@@ -0,0 +1,68 @@
+let racingTimings = [
+    {
+        "Track": "Istanbul Park",
+        "Date": "01-10-2024",
+        "Time": "09:25:00",
+        "Driver": "Max Verstappen",
+        "Car": "Opel Corsa",
+        "Car Drivetrain": "FWD",
+        "Car Weight (kg)": 1200,
+        "Car Horsepower (hp)": 100,
+        "Lap Time (ms)": 81026
+    },
+    {
+        "Track": "Istanbul Park",
+        "Date": "01-10-2024",
+        "Time": "09:30:00",
+        "Driver": "Lewis Hamilton",
+        "Car": "Ford Fiesta",
+        "Car Drivetrain": "FWD",
+        "Car Weight (kg)": 1100,
+        "Car Horsepower (hp)": 90,
+        "Lap Time (ms)": 82026
+    },
+    {
+        "Track": "Istanbul Park",
+        "Date": "01-10-2024",
+        "Time": "09:35:00",
+        "Driver": "Valtteri Bottas",
+        "Car": "Volkswagen Golf",
+        "Car Drivetrain": "FWD",
+        "Car Weight (kg)": 1300,
+        "Car Horsepower (hp)": 110,
+        "Lap Time (ms)": 80026
+    },
+    {
+        "Track": "Istanbul Park",
+        "Date": "01-10-2024",
+        "Time": "09:40:00",
+        "Driver": "Charles Leclerc",
+        "Car": "Audi A3",
+        "Car Drivetrain": "FWD",
+        "Car Weight (kg)": 1250,
+        "Car Horsepower (hp)": 105,
+        "Lap Time (ms)": 80526
+    },
+    {
+        "Track": "Istanbul Park",
+        "Date": "01-10-2024",
+        "Time": "09:45:00",
+        "Driver": "Carlos Sainz",
+        "Car": "Renault Clio",
+        "Car Drivetrain": "FWD",
+        "Car Weight (kg)": 1150,
+        "Car Horsepower (hp)": 95,
+        "Lap Time (ms)": 81526
+    },
+    {
+        "Track": "Nordschleife",
+        "Date": "05-10-2024",
+        "Time": "09:45:00",
+        "Driver": "Batuhan Erdeniz",
+        "Car": "BMW 325i",
+        "Car Drivetrain": "RWD",
+        "Car Weight (kg)": 1250,
+        "Car Horsepower (hp)": 195,
+        "Lap Time (ms)": 501526
+    }
+];
